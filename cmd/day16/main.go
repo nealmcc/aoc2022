866d6a1588c703a12a233a8e3c072284b99484da
Neal McConachie <neal@mcconachie.co>
2022-12-18 22:30:04 +0000

    day 16: Solve part 2

--- cmd/day16/main.go
+++ cmd/day16/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"log"
 	"os"
 	"time"
+
+	pq "github.com/nealmcc/aoc2022/pkg/collection/prioqueue"
 )
 
 func main() {
@@ -23,24 +26,19 @@ func main() {
 	p1 := part1(graph)
 	middle := time.Now()
 
-	// p2 := part2(valves)
-	// end := time.Now()
+	p2 := part2(graph)
+	end := time.Now()
 
 	fmt.Printf("part 1: %d in %s\n", p1, middle.Sub(start))
-	// fmt.Printf("part 2: %d in %s\n", p2, end.Sub(middle))
+	fmt.Printf("part 2: %d in %s\n", p2, end.Sub(middle))
 }
 
-// state1 is a comparable struct that allows us to reduce the search space
-// for each turn during part1.
-// See https://go.dev/ref/spec for more about 'comparable'.
-type state1 struct {
-	key1
-	score int
-}
-
-type key1 struct {
-	ix   int    // current position
-	mask uint64 // which valves are currently open, expressed as a bitmask
+// state is a comparable struct that allows us to reduce the search space.
+// See https://go.dev/ref/spec for more about 'comparable' in Go.
+type state struct {
+	pos1       int    // current position
+	pos2       int    // current position of elephant (part 2)
+	openValves uint64 // which valves are currently open, expressed as a bitmask
 }
 
 // part1 solves part 1 of the puzzle
@@ -51,7 +49,7 @@ type key1 struct {
 // would have the same current position and open valves, but less accumulated
 // flow, we significantly reduce the search space for the next step.
 //
-// In order to do so, we need the stateKeys for a given turn to be comparable.
+// In order to do so, we need the states for a given turn to be comparable.
 // That is, they must support the == and != operations.
 //
 // Due to the above requirement, we can't use a map or a slice to store the set
@@ -67,53 +65,175 @@ func part1(input map[ValveID]*Valve) int {
 		return input[k].ix
 	}
 
-	t := 0 // turn number (aka minutes elapsed)
-
-	buffers := [][]state1{
-		make([]state1, 0, 2048),
-		make([]state1, 0, 2048),
+	// each round of evaluation uses a new queue.  This is the first.
+	qFirst := new(pq.Queue[state])
+	q := &qFirst
+	start := &pq.Node[state]{
+		Value:    state{pos1: getIndex(K("AA"))},
+		Priority: 0,
 	}
 
-	// For each turn, all states and best score for each
-	states := append(buffers[0], state1{key1: key1{ix: getIndex(K("AA"))}})
-	best := make(map[key1]int, 100000)
-	for t = 1; t < limit; t++ {
-		newstates := buffers[t%2][:0]
-		for _, next := range states {
-			k := key1{ix: next.ix, mask: next.mask}
-			hiscore, ok := best[k]
-			if ok && hiscore >= next.score {
-				continue
-			}
-			best[k] = next.score
+	best := make(map[state]int)
+	heap.Push(*q, start)
+	for t := 1; t < limit; t++ {
+		qNext := new(pq.Queue[state])
+		pNext := make(map[state]*pq.Node[state])
 
-			flow := valves[next.ix].Flow
-			mask := uint64(1) << next.ix
-			if flow > 0 && next.mask&mask == 0 {
-				newstates = append(newstates, state1{
-					key1:  key1{ix: next.ix, mask: next.mask | mask},
-					score: next.score + (limit-t)*flow,
-				})
+		for (*q).Len() > 0 {
+			curr := heap.Pop(*q).(*pq.Node[state])
+
+			flow := valves[curr.Value.pos1].Flow
+			mask := uint64(1) << curr.Value.pos1
+			canOpen := flow > 0 && curr.Value.openValves&mask == 0
+			neighbours := valves[curr.Value.pos1].Neighbours
+
+			for _, n := range neighbours {
+				ix := getIndex(n)
+				k := state{pos1: ix, openValves: curr.Value.openValves}
+				upsert(qNext, &best, &pNext, k, curr.Priority)
 			}
 
-			for _, neighbour := range valves[next.ix].Neighbours {
-				newstates = append(newstates, state1{
-					key1:  key1{ix: input[neighbour].ix, mask: next.mask},
-					score: next.score,
-				})
+			if canOpen {
+				// add a state where we open this valve
+				k := state{pos1: curr.Value.pos1, openValves: curr.Value.openValves | mask}
+				score := curr.Priority + (limit-t)*flow
+				upsert(qNext, &best, &pNext, k, score)
 			}
 
-			states = newstates
 		}
+		*q = qNext
 	}
 
 	max := 0
-	for _, s := range states {
-		if s.score > max {
-			max = s.score
+	for _, v := range best {
+		if v > max {
+			max = v
 		}
 	}
 	return max
+}
+
+// part 2 works similarly to part 1, except that we track the elephant's
+// position as well as our own.
+func part2(input map[ValveID]*Valve) int {
+	const limit = 26
+	_, valves := index(input)
+
+	getIndex := func(k ValveID) int {
+		return input[k].ix
+	}
+
+	// each round of evaluation uses a new queue.  This is the first.
+	qFirst := new(pq.Queue[state])
+	q := &qFirst
+	start := &pq.Node[state]{
+		Value: state{
+			pos1: getIndex(K("AA")),
+			pos2: getIndex(K("AA")),
+		},
+		Priority: 0,
+	}
+
+	best := make(map[state]int, 13000000)
+	heap.Push(*q, start)
+	for t := 1; t < limit; t++ {
+		qNext := new(pq.Queue[state])
+		pNext := make(map[state]*pq.Node[state])
+
+		for (*q).Len() > 0 {
+			curr := heap.Pop(*q).(*pq.Node[state])
+
+			flow1 := valves[curr.Value.pos1].Flow
+			mask1 := uint64(1) << curr.Value.pos1
+			canOpen1 := flow1 > 0 && curr.Value.openValves&mask1 == 0
+			neighbours1 := valves[curr.Value.pos1].Neighbours
+
+			flow2 := valves[curr.Value.pos2].Flow
+			mask2 := uint64(1) << curr.Value.pos2
+			canOpen2 := flow2 > 0 && curr.Value.openValves&mask2 == 0
+			neighbours2 := valves[curr.Value.pos2].Neighbours
+
+			if canOpen1 && canOpen2 {
+				// add a state where we both open the valve in our current room
+				k := state{curr.Value.pos1, curr.Value.pos2, curr.Value.openValves | mask1 | mask2}
+				score := curr.Priority + (limit-t)*(flow1+flow2)
+				upsert(qNext, &best, &pNext, k, score)
+			}
+
+			if canOpen1 {
+				// add states where I open the valve, but the elephant moves
+				for _, n2 := range neighbours2 {
+					ix := getIndex(n2)
+					if ix != curr.Value.pos1 {
+						k := state{curr.Value.pos1, ix, curr.Value.openValves | mask1}
+						score := curr.Priority + (limit-t)*flow1
+						upsert(qNext, &best, &pNext, k, score)
+					}
+				}
+			}
+
+			if canOpen2 {
+				// add states where the elephant opens the valve, but I move
+				for _, n1 := range neighbours1 {
+					ix := getIndex(n1)
+					if ix != curr.Value.pos2 {
+						k := state{ix, curr.Value.pos2, curr.Value.openValves | mask2}
+						score := curr.Priority + (limit-t)*flow2
+						upsert(qNext, &best, &pNext, k, score)
+					}
+				}
+			}
+
+			// we both move
+			for _, n1 := range neighbours1 {
+				ix1 := getIndex(n1)
+				for _, n2 := range neighbours2 {
+					ix2 := getIndex(n2)
+					if ix1 == ix2 {
+						continue
+					}
+					k := state{ix1, ix2, curr.Value.openValves}
+					upsert(qNext, &best, &pNext, k, curr.Priority)
+				}
+			}
+		}
+
+		*q = qNext
+	}
+
+	max := 0
+	for _, v := range best {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+// upsert checks to see if the given key, score combination beats what currently exists
+// within the map 'best'. If so, it will either add or update the value in the queue.
+// Performs some optimisations to avoid duplicate state in the queue.
+func upsert(q *pq.Queue[state], best *map[state]int, pointers *map[state]*pq.Node[state], key state, score int) {
+	if key.pos2 != 0 && key.pos1 > key.pos2 {
+		key.pos1, key.pos2 = key.pos2, key.pos1
+	}
+
+	if sc, ok := (*best)[key]; ok && score <= sc {
+		return
+	}
+	(*best)[key] = score
+
+	p, ok := (*pointers)[key]
+	if !ok {
+		p = &pq.Node[state]{
+			Value:    key,
+			Priority: score,
+		}
+		heap.Push(q, p)
+		(*pointers)[key] = p
+	} else {
+		q.Update(p, key, score)
+	}
 }
 
 // index organises the input data so it's more efficient to work with.
--- cmd/day16/main_test.go
+++ cmd/day16/main_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestPart1(t *testing.T) {
+func TestPart1_sample(t *testing.T) {
 	t.Parallel()
 
 	valves, err := ReadValves(strings.NewReader(_sample))
@@ -22,6 +22,54 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2_sample(t *testing.T) {
+	t.Parallel()
+
+	valves, err := ReadValves(strings.NewReader(_sample))
+	if err != nil {
+		t.Log("error reading sample:", err)
+		t.FailNow()
+	}
+
+	got, want := part2(valves), 1707
+	if got != want {
+		t.Logf("part2() = %d; want %d", got, want)
+		t.Fail()
+	}
+}
+
+func TestPart1_actual(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
+	}
+	t.Parallel()
+
+	file, _ := os.Open("input.txt")
+	valves, _ := ReadValves(file)
+
+	got, want := part1(valves), 1659
+	if got != want {
+		t.Logf("part1() = %d; want %d", got, want)
+		t.Fail()
+	}
+}
+
+func TestPart2_actual(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
+	}
+	t.Parallel()
+
+	file, _ := os.Open("input.txt")
+	valves, _ := ReadValves(file)
+
+	got, want := part2(valves), 2382
+	if got != want {
+		t.Logf("part2() = %d; want %d", got, want)
+		t.Fail()
+	}
+}
+
 var _p1 int
 
 func BenchmarkPart1(b *testing.B) {
@@ -33,4 +81,17 @@ func BenchmarkPart1(b *testing.B) {
 		p1 = part1(valves)
 	}
 	_p1 = p1
+}
+
+var _p2 int
+
+func BenchmarkPart2(b *testing.B) {
+	file, _ := os.Open("input.txt")
+	valves, _ := ReadValves(file)
+	b.ResetTimer()
+	var p2 int
+	for i := 0; i < b.N; i++ {
+		p2 = part2(valves)
+	}
+	_p2 = p2
 }
